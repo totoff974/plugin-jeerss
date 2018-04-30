6c31e1089025a51430129c00839a2bb04d20846d
totoff974 <totoff974@users.noreply.github.com>
2018-04-30 15:45:02 +0400

    prise en charge plusieurs info

--- core/config/JeeRss.config.php
+++ core/config/JeeRss.config.php
@@ -20,21 +20,112 @@ global $listCmdJeeRss;
 
 
 $listCmdJeeRss = array(
-
+	
     array(
         'name' => 'Dernier Titre',
         'type' => 'info',
         'subType' => 'string',
+		'order' => 1,
 		'configuration' => array(
 			'titre' => '',
         ),
     ),
 	
     array(
+        'name' => 'Titre 9',
+        'type' => 'info',
+        'subType' => 'string',
+		'order' => 2,
+		'configuration' => array(
+			'titre' => '',
+        ),
+    ),
+
+    array(
+        'name' => 'Titre 8',
+        'type' => 'info',
+        'subType' => 'string',
+		'order' => 3,
+		'configuration' => array(
+			'titre' => '',
+        ),
+    ),
+	
+    array(
+        'name' => 'Titre 7',
+        'type' => 'info',
+        'subType' => 'string',
+		'order' => 4,
+		'configuration' => array(
+			'titre' => '',
+        ),
+    ),
+	
+    array(
+        'name' => 'Titre 6',
+        'type' => 'info',
+        'subType' => 'string',
+		'order' => 5,
+		'configuration' => array(
+			'titre' => '',
+        ),
+    ),
+	
+    array(
+        'name' => 'Titre 5',
+        'type' => 'info',
+        'subType' => 'string',
+		'order' => 6,
+		'configuration' => array(
+			'titre' => '',
+        ),
+    ),
+	
+    array(
+        'name' => 'Titre 4',
+        'type' => 'info',
+        'subType' => 'string',
+		'order' => 7,
+		'configuration' => array(
+			'titre' => '',
+        ),
+    ),
+	
+    array(
+        'name' => 'Titre 3',
+        'type' => 'info',
+        'subType' => 'string',
+		'order' => 8,
+		'configuration' => array(
+			'titre' => '',
+        ),
+    ),
+	
+    array(
+        'name' => 'Titre 2',
+        'type' => 'info',
+        'subType' => 'string',
+		'order' => 9,
+		'configuration' => array(
+			'titre' => '',
+        ),
+    ),
+	
+    array(
+        'name' => 'Titre 1',
+        'type' => 'info',
+        'subType' => 'string',
+		'order' => 10,
+		'configuration' => array(
+			'titre' => '',
+        ),
+    ),
+	
+	array(
         'name' => 'Refresh',
         'type' => 'action',
         'subType' => 'other',
-    ),	
+		'order' => 11,
+    ),
+	
 );
-
-?>
